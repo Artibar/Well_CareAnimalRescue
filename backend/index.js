3f476c45df311e812ad9daf6c2fdd69520cd04fc
Arti Barsagade <artibarsagade2000@gmail.com>
2025-12-14 16:45:55 +0530

    fix: change catch-all route from /* to /(.*)/

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(cors(
-    { origin:"/*" },
+    { origin:/(.*)/ },
     { Credential: true }
 ))
 app.use(express.json())
@@ -42,7 +42,7 @@ app.use("/adoption", adoptionRouter)
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-  app.get('/*', (req, res)=>{
+  app.get(/(.*)/, (req, res)=>{
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
   })
 }
